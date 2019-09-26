8698c68132c9a52e87a2d2a46d1908b40d6781c7
Laksono Bramantio <laxbramantio@gmail.com>
2019-09-26 13:18:10 +0700

    chore: Edit RestoranService and Add MenuService

--- gopud/src/main/java/apap/tutorial/gopud/service/RestoranService.java
+++ gopud/src/main/java/apap/tutorial/gopud/service/RestoranService.java
@@ -1,6 +1,8 @@
 package apap.tutorial.gopud.service;
 
 import java.util.List;
+import java.util.Optional;
+
 import apap.tutorial.gopud.model.RestoranModel;
 
 public interface RestoranService {
@@ -10,6 +12,7 @@ public interface RestoranService {
     // Method untuk mendapatkan semua data Restoran yang tersimpan
     List<RestoranModel> getRestoranList();
 
-    // Method untuk mendapatkan data sebuah Restoran berdasarkan idRestoran
-    RestoranModel getRestoranByIdRestoran(String idRestoran);
+    Optional<RestoranModel> getRestoranByIdRestoran(Long idRestoran);
+
+    RestoranModel changeRestoran(RestoranModel restoranModel);
 }
