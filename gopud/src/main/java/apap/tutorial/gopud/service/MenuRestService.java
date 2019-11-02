d59f6b4a87bb5b4d931978236b55903bb17dc4f8
Laksono Bramantio <laxbramantio@gmail.com>
2019-11-02 23:03:13 +0700

    feat: Finish Latihan No. 1

---
+++ gopud/src/main/java/apap/tutorial/gopud/service/MenuRestService.java
@@ -0,0 +1,13 @@
+package apap.tutorial.gopud.service;
+
+import apap.tutorial.gopud.model.MenuModel;
+
+import java.util.List;
+
+public interface MenuRestService {
+    MenuModel createMenu(MenuModel menu);
+    List<MenuModel> retrieveListMenu();
+    MenuModel getMenuByIdMenu(Long idMenu);
+    MenuModel changeMenu(Long idMenu, MenuModel menuUpdate);
+    void deleteMenu(Long idMenu);
+}
