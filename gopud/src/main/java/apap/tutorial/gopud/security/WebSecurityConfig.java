0e191d8e48adc0a7d908cd657ca74344b70f9b76
Laksono Bramantio <laxbramantio@gmail.com>
2019-11-06 22:20:43 +0700

    fix: Fix WebSecurityConfig for addUser feature

--- gopud/src/main/java/apap/tutorial/gopud/security/WebSecurityConfig.java
+++ gopud/src/main/java/apap/tutorial/gopud/security/WebSecurityConfig.java
@@ -22,7 +22,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/css/**").permitAll()
                 .antMatchers("/js/**").permitAll()
                 .antMatchers("/restoran/**").hasAnyAuthority("MERCHANT")
-                .antMatchers("/user/**").hasAnyAuthority("ADMIN")
+                .antMatchers("/user/addUser").hasAnyAuthority("ADMIN")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
