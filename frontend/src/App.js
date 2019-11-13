0c62615a11e578bf492d41af23976787907e0c19
Laksono Bramantio <laxbramantio@gmail.com>
2019-11-13 22:28:33 +0700

    feat: Finish tutorial 8

---
+++ frontend/src/App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import List from "./components/List";
+import dummyItems from "./items.json"
+
+export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      favItems: []
+    };
+  }
+  render() {
+    const { favItems } = this.state;
+    return (
+      <div className="container-fluid">
+        <h1 className="text-center">
+          Welcome!
+          <small>Class-based</small>
+        </h1>
+        <div className="container pt-3">
+          <div className="row">
+            <div className="col-sm">
+              <List
+                title="Our Product"
+                items={dummyItems}
+                onItemClick={this.handleItemClick}
+              />
+            </div>
+            <div className="col-sm">
+              <List
+                title="My Favourite"
+                items={favItems}
+                onItemClick={this.handleItemClick}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  handleItemClick = item => {
+    const newItems = [...this.state.favItems];
+    const newItem = {...item};
+    const targetInd = newItems.findIndex(it => it.id === newItem.id);
+    if(targetInd < 0) newItems.push(newItem);
+    else newItems.splice(targetInd, 1);
+    this.setState({ favItems: newItems });
+  };
+}
---
+++ frontend/src/components/List.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import Item from "./Item"
+
+export default function List({ title, items, onItemClick}){
+    return(
+        <>
+            <h3 style={style.heading}>{title}</h3>
+            <div className="list-group">
+                {items.map(item => (
+                    <Item key={item.id} item={item} onChange={onItemClick}/>
+                ))}
+            </div>
+        </>
+
+    );
+}
+
+const style = {
+    heading: {
+        fontFamily: "courier new"
+    }
+};
